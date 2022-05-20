eea090e05af6b6f48a51f2db8dbb4206048c8579
idesignerSe <hebera75@gmail.com>
2022-05-20 09:01:25 +0200

    Update ViewChecklist.js
    look code of the modal

--- public/views/ViewChecklist.js
+++ public/views/ViewChecklist.js
@@ -100,7 +100,27 @@ export default class ViewChecklist {
                       <td>
                         <button type="button" id="notering" class="btn btn-notering" data-toggle="modal" data-target="#exampleModal">
                           Yes!
-                        </button>               
+                        </button>
+                        <!-- Modal -->
+                        <div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+                          <div class="modal-dialog modal-dialog-centered" role="document">
+                            <div class="modal-content">
+                              <div class="modal-header">
+                                <h5 class="modal-title" id="exampleModalLongTitle">Modal title</h5>
+                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                  <span aria-hidden="true">&times;</span>
+                                </button>
+                              </div>
+                              <div class="modal-body">
+                                ...
+                              </div>
+                              <div class="modal-footer">
+                                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                                <button type="button" class="btn btn-primary">Save changes</button>
+                              </div>
+                            </div>
+                          </div>
+                        </div>               
                       </td>
                     </tr>
                     <tr>
