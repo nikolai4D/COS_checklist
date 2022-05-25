a3ee0d3add90f0846e8ad3a9b33859d693c8f245
idesignerSe <hebera75@gmail.com>
2022-05-25 16:10:13 +0200

    Trying to solve the date
    just creating a function and looking how to print. /Heb

--- public/views/Dashboard.js
+++ public/views/Dashboard.js
@@ -8,6 +8,21 @@ export default class Dashboard {
     document.title = "Checklist - Dashboard";
   }
 
+  function formatDate(date) {
+    var d = new Date(date),
+        year = d.getFullYear(),
+        month = '' + (d.getMonth() + 1),
+        day = '' + d.getDate();
+
+    if (month.length < 2) 
+        month = '0' + month;
+    if (day.length < 2) 
+        day = '0' + day;
+
+    return [year, month, day].join('-');
+}
+ 
+console.log(formatDate('Sun May 11,2014'));
   async getAllChecklists() {
      await Merchant.getAllDetailedDataOfType(Librarian.checklist.type)
 
