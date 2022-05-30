9f24daf229d362fd60fa29ca501a602a7bacf1ec
Maja Frankiewicz <majafrank@live.se>
2022-05-30 19:35:05 +0200

    wip: beginning of validation

--- public/helpers/checklistDetails/validateChecklist.js
+++ public/helpers/checklistDetails/validateChecklist.js
@@ -1,4 +1,6 @@
 import {State} from "../../store/State.js";
+import Merchant from "../../store/Merchant";
+import { Librarian } from "../../store/Librarian";
 
 export default function (e) {
 
@@ -16,4 +18,13 @@ if (nonSelected.length > 0) {
     alert("Fyll i följande frågor för att skicka in: " + nonSelected.map(question => question.title).join(", "))
     return
 }
+
+let validation = "Approved"
+for (const answer of activeChecklist.questions){
+    for (const question of answer.questions){
+        if(!question.status) validation = "Not approved";
+    }
+}
+console.log(validation)
+// Merchant.updateData(activeChecklist)
 }
