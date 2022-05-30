02d638649242e5670c8702dcd0b4c9d99b12c014
Maja Frankiewicz <majafrank@live.se>
2022-05-30 13:30:12 +0200

    fix: DOM updated when deleting

--- public/helpers/checklistOverview/deleteChecklist.js
+++ public/helpers/checklistOverview/deleteChecklist.js
@@ -1,11 +1,13 @@
 import navigateTo from "../navigateTo.js";
 import Merchant from "../../store/Merchant.js";
+import { State } from "../../store/State.js";
 
 export default async function (e) {
     console.log("delete checklist fonction called")
     const checkListId = e.target.parentElement.parentElement.getAttribute("data-id")
 
     await Merchant.deleteData({type: "checklist", id: checkListId})
+    State.allChecklistsWithDetails.content.allChecklistsFormatted = State.allChecklistsWithDetails.content.allChecklistsFormatted.filter(checklist => checklist.id !== checkListId)
 
     navigateTo("/")
 }
