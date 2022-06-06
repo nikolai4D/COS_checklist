7bad0cce657312170ba7c74b511dab7d792f4b69
Maja Frankiewicz <majafrank@live.se>
2022-06-06 14:32:45 +0200

    wip: added modal to detailed checklist

--- public/components/Question.js
+++ public/components/Question.js
@@ -35,8 +35,36 @@ export default  function (question, number) {
             ${options}
         </select>
     </td>
-    <td>...</td>
-    <td> <label data-function="upload-images" id="labelForInputImage_$${question.id}" for="inputImage_$${question.id}" class="btn btn-outline-secondary"><i class="bi bi-upload"></i></label><input class="form-control" data-function="upload-images" type="file" id="inputImage_$${question.id}" style="display:none;"  accept="image/*" >
-    </td>
+    <td>
+        <button type="button" class="btn btn-outline-secondary position-relative" data-bs-toggle="modal" data-bs-target="#modal_${question.id}">🖊
+        <span class="position-absolute top-0 start-100 translate-middle p-2 bg-danger border border-light rounded-circle">
+        <span class="visually-hidden">New alerts</span>
+      </span>
+      </button>
+        <div class="modal fade" id="modal_${question.id}" tabindex="-1" aria-labelledby="modal_${question.id}" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="modal_${question.id}">Ny notering</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <form>
+                <div class="mb-3">
+                    <label for="modalinput_${question.id}" class="col-form-label">Notering:</label>
+                    <textarea class="form-control" id="modalinput_${question.id}"></textarea>
+                </div>
+                </form>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                <button type="button" class="btn btn-primary">Spara</button>
+            </div>
+            </div>
+        </div>
+        </div>
+        </td>
+        <td> <label data-function="upload-images" id="labelForInputImage_$${question.id}" for="inputImage_$${question.id}" class="btn btn-outline-secondary"><i class="bi bi-upload"></i></label><input class="form-control" data-function="upload-images" type="file" id="inputImage_$${question.id}" style="display:none;"  accept="image/*" >
+        </td>
     </tr>`;
 };
