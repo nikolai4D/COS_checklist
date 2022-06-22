a4b6b1264f9ef7e194226efdeed86fbee9e683a2
mathieu <mathieuantoine4@gmail.com>
2022-06-22 13:15:45 +0200

    Sending picture from client to server to db.

--- api/routers/pictureRouter.js
+++ api/routers/pictureRouter.js
@@ -2,28 +2,31 @@ const express = require("express");
 const multer = require("multer");
 const router = express.Router();
 const fs = require("fs");
+const FormData = require('form-data');
+const axios = require("axios");
 require("dotenv").config();
 
-const bodyParser = require("body-parser");
-const { apiCallPost, apiCallGet, apiCallDelete } = require("./helpers");
 
 const upload = multer({ storage: multer.memoryStorage() })
 
-//Bodyparser
-//router.use(bodyParser.json());
-
-router.post("/",  upload.single ('answer_picture'),(req, res) => {
+router.post("/",  upload.single ('answer_picture'),async (req, res) => {
     console.log("picture route used");
 
-    // Open file as a readable stream
-    //const fileStream = fs.createReadStream('./large-file.zip');
-
-    //const form = new FormData();
-    // Pass file stream directly to form
-    //form.append('largeFile', fileStream, 'large-file.zip');
-
     console.log("file: " + JSON.stringify(req.file))
-    res.json({msg:"we are cool now"})
+
+    const form = new FormData();
+    const file = req.file;
+    form.append('answer_picture', file.buffer, file.originalname);
+
+    console.log("form: " + JSON.stringify(form))
+    const response = await axios.post(process.env.API_BASE_URL + "/assets" , form, {
+        withCredentials: true,
+        headers: {
+            ...form.getHeaders(),
+            apikey: process.env.API_KEY}
+    });
+
+    res.json({msg: "we are cool now"})
 })
 
 module.exports = router;
