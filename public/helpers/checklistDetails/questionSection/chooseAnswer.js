e56e48eaad73046d5a305ee0426ae0e2743c8943
Maja Frankiewicz <majafrank@live.se>
2022-08-14 19:16:02 +0200

    fix: spinner and not applicable no color

--- public/helpers/checklistDetails/questionSection/chooseAnswer.js
+++ public/helpers/checklistDetails/questionSection/chooseAnswer.js
@@ -1,20 +1,20 @@
-import {State} from "../../../store/State.js";
+import { State } from "../../../store/State.js";
 
 export default function (e) {
-let selectedAnswer = e.target.value;
-let selectedQuestion = e.target.parentNode;
-let selectedQuestionGroup = e.target.parentNode.parentNode.parentNode.parentNode.id
-selectedQuestion.parentNode.parentNode.classList.remove("table-success")
-selectedQuestion.parentNode.parentNode.classList.remove("table-danger")
+    let selectedAnswer = e.target.value;
+    let selectedQuestion = e.target.parentNode;
+    let selectedQuestionGroup = e.target.parentNode.parentNode.parentNode.parentNode.id
+    selectedQuestion.parentNode.parentNode.classList.remove("table-success")
+    selectedQuestion.parentNode.parentNode.classList.remove("table-danger")
 
-let activeChecklist = State.activeChecklist.content;
-let questionGroup = activeChecklist.questions.find(questionGroup => questionGroup.id === selectedQuestionGroup)
-let question = questionGroup.questions.find(question => question.id === selectedQuestion.id)
-question.selectedAnswer = selectedAnswer;
-let status = question.status = question.answers.preferredAnswer.id === selectedAnswer;
-question.status = status;
-
-if (question.answers.possibleAnswers.find(answer => selectedAnswer === answer.id).title ===  "N/A"){question.status = true; return;}
-if (status){ selectedQuestion.parentNode.parentNode.classList.add("table-success"); return; }
-selectedQuestion.parentNode.parentNode.classList.add("table-danger")
+    let activeChecklist = State.activeChecklist.content;
+    let questionGroup = activeChecklist.questions.find(questionGroup => questionGroup.id === selectedQuestionGroup)
+    let question = questionGroup.questions.find(question => question.id === selectedQuestion.id)
+    question.selectedAnswer = selectedAnswer;
+    let status = question.status = question.answers.preferredAnswer.id === selectedAnswer;
+    question.status = status;
+    let isNotApplicable = question.answers.possibleAnswers.find(answer => selectedAnswer === answer.id).title === "Ej tillämpbar"
+    if (isNotApplicable) { question.status = true; return; }
+    if (status) { selectedQuestion.parentNode.parentNode.classList.add("table-success"); return; }
+    selectedQuestion.parentNode.parentNode.classList.add("table-danger")
 }
--- public/store/Librarian.js
+++ public/store/Librarian.js
@@ -1,5 +1,11 @@
 export const Librarian = {
-    checklist: { type: "checklist" }, question: { type: "question" }, address: { type: "address" }, answer: { type: "answer" },
+    checklist: { type: "checklist" }, question: { type: "question" }, address: { type: "address" }, answer: {
+        type: "answer", possibleAnswers: [
+            { sv: "Ja", eng: "Yes" },
+            { sv: "Nej", eng: "No" },
+            { sv: "Ej tillämpbar", eng: "N/A" }
+        ]
+    },
     status: [
         { sv: "Godkänd", eng: "Approved" },
         { sv: "Ej godkänd", eng: "Not approved" },
