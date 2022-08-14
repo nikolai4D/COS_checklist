c8829e50cf311485614af19c97412091f3466375
Maja Frankiewicz <majafrank@live.se>
2022-08-14 11:15:38 +0200

    refactor: delete feat

--- public/helpers/checklistDetails/questionSection/deleteImage.js
+++ public/helpers/checklistDetails/questionSection/deleteImage.js
@@ -3,32 +3,27 @@ import Merchant from "../../../store/Merchant.js";
 import { State } from "../../../store/State.js";
 
 export default async function (e) {
-    console.log(e)
-    let deleteButtonId = e.target.id;
-    let questionId = e.target.parentNode.parentNode.getAttribute("data-id")
-    let { question } = getSelectedQuestion(e, questionId)
-    delete question.image;
+    const deleteButtonId = e.target.id;
+    const questionId = e.target.parentNode.parentNode.getAttribute("data-id")
+    const imageId = deleteButtonId.split("$")[1]
 
-
-    let imageId = deleteButtonId.split("$")[1]
-    document.getElementById(`buttonForImage_$${imageId}`).remove();
-    e.target.remove()
-    // console.log(questionId, document.getElementById(`inputImage_$${questionId}`))
-    document.getElementById(`inputImage_$${questionId}`).value = null;
-
-    let activeChecklist = State.activeChecklist.content;
-    console.log(activeChecklist, "sTATE")
-
+    removeFromState(e, questionId);
+    removeFromClient(imageId, e, questionId);
     await Merchant.DELETE_PICTURE(imageId)
 
-    // console.log(imageId, "imageID")
-    // console.log(e.target.parentElement.parentElement)
-    // document.getElementById(`buttonForImage_$as_${imageId}`).remove();
-    // e.target.remove()
-    // document.getElementById(`deleteImage_$${imageId}`).remove();
-    // document.getElementById(`inputImage_$${fragaId}`).value = null;
 }
 
+
+function removeFromClient(imageId, e, questionId) {
+    document.getElementById(`buttonForImage_$${imageId}`).remove();
+    e.target.remove();
+    document.getElementById(`inputImage_$${questionId}`).value = null;
+}
+
+function removeFromState(e, questionId) {
+    let { question } = getSelectedQuestion(e, questionId);
+    delete question.image;
+}
 
 function getSelectedQuestion(e, questionId) {
     let selectedQuestionGroup = e.target.parentNode.parentNode.parentNode;
