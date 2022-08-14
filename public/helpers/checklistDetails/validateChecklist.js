fed4d98acacfcb599cb5d43aade666d278b6bf59
Maja Frankiewicz <majafrank@live.se>
2022-08-14 14:16:34 +0200

    fix: validates address if update

--- public/helpers/checklistDetails/validateChecklist.js
+++ public/helpers/checklistDetails/validateChecklist.js
@@ -1,4 +1,4 @@
-import {State} from "../../store/State.js";
+import { State } from "../../store/State.js";
 import Merchant from "../../store/Merchant.js";
 import { Librarian } from "../../store/Librarian.js";
 import navigateTo from "../navigateTo.js";
@@ -6,41 +6,41 @@ import navigateTo from "../navigateTo.js";
 
 export default async function (e) {
 
-let nonSelected = []
+    let nonSelected = []
 
-let activeChecklist = State.activeChecklist.content
-for (const answer of activeChecklist.questions){
-    for (const question of answer.questions){
-        if (!question.selectedAnswer){
-            nonSelected.push(question)
+    let activeChecklist = State.activeChecklist.content
+    for (const answer of activeChecklist.questions) {
+        for (const question of answer.questions) {
+            if (!question.selectedAnswer) {
+                nonSelected.push(question)
+            }
         }
     }
-}
-let answeredAddress = activeChecklist.address.title !== "-"
+    let answeredAddress = typeof activeChecklist.address !== "undefined" && activeChecklist.address.title !== "-";
 
-if (nonSelected.length > 0) {
-    alert("Fyll i följande frågor för att skicka in: " + nonSelected.map(question => question.title).join(", "))
-    return
-}
- if (!answeredAddress){
-    alert("Fyll i Område, Fastighet och Adress")
-    return
-}
-
-let isApproved = true;
-for (const answer of activeChecklist.questions){
-    for (const question of answer.questions){
-        if(!question.status) isApproved = false;
+    if (nonSelected.length > 0) {
+        alert("Fyll i följande frågor för att skicka in: " + nonSelected.map(question => question.title).join(", "))
+        return
     }
-}
-alert(`Checklist validated to: ${isApproved? "approved" : "not approved"}`)
+    if (!answeredAddress) {
+        alert("Fyll i Område, Fastighet och Adress")
+        return
+    }
+
+    let isApproved = true;
+    for (const answer of activeChecklist.questions) {
+        for (const question of answer.questions) {
+            if (!question.status) isApproved = false;
+        }
+    }
+    alert(`Checklist validated to: ${isApproved ? "approved" : "not approved"}`)
 
 
-let existingChecklist = State.allChecklistsWithDetails.content.allChecklistsFormatted.find(checklist => checklist.id === activeChecklist.id);
-if (!existingChecklist) await Merchant.createData({type:Librarian.answer.type, activeChecklist});
-else await Merchant.updateData({type:Librarian.answer.type, activeChecklist});
-await Merchant.updateData({type: Librarian.checklist.type, activeChecklist, isApproved})
+    let existingChecklist = State.allChecklistsWithDetails.content.allChecklistsFormatted.find(checklist => checklist.id === activeChecklist.id);
+    if (!existingChecklist) await Merchant.createData({ type: Librarian.answer.type, activeChecklist });
+    else await Merchant.updateData({ type: Librarian.answer.type, activeChecklist });
+    await Merchant.updateData({ type: Librarian.checklist.type, activeChecklist, isApproved })
 
-navigateTo('/')
+    navigateTo('/')
 
 }
